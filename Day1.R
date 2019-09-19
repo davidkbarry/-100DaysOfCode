4db995da424fade387fd5b7d37b1d0fab800120f
David Barry <55557733+davidkbarry@users.noreply.github.com>
2019-09-19 16:49:02 -0500

    Add files via upload
    Day 1/100: Code to plot all balls and strikes called for the previous day. Uses; Bill Petti's (@BillPetti on Twitter) Statcast scraper, and Lukas PM Kremer's (@LPMKremer on Twitter) geom_pointdensity() function.

---
+++ Day1.R
@@ -0,0 +1,29 @@
+
+#DAY1CODE
+
+#DEFINE YESTERDAY
+yesterday<- Sys.Date()-1
+
+#GET YESTERDAY'S GAMES FROM STATCAST
+yesterdaygames<-scrape_statcast_savant(start_date = yesterday, end_date = yesterday)
+
+#PLOT CALLED STRIKES
+yesterdaystrikes<- subset(yesterdaygames, description=='called_strike')
+plotPITCHstrike = ggplot(yesterdaystrikes, aes(plate_x, plate_z))+
+  geom_pointdensity() +
+  xlim(c(-3, 3))+ylim(c(0,6))+  
+  scale_color_viridis(option="A") +
+  geom_rect(data=NULL, mapping=aes(xmin=-((17/2)/12), xmax=((17/2)/12), ymin=1.5, ymax=3.5),size=2, color="tomato", alpha=0.005)
+plotPITCHstrike
+
+#PLOT CALLED BALLS
+yesterdayballs<- subset(yesterdaygames, description=='ball')
+plotPITCHballs = ggplot(yesterdayballs, aes(plate_x, plate_z))+
+  geom_pointdensity() +
+  xlim(c(-3, 3))+ylim(c(0,6))+  
+  scale_color_viridis(option="D") + theme(legend.position="right")+
+  geom_rect(data=NULL, mapping=aes(xmin=-((17/2)/12), xmax=((17/2)/12), ymin=1.5, ymax=3.5),size=2, color="tomato", alpha=0.005)
+plotPITCHballs
+
+
+
